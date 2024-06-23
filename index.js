bbd6c12f31da6cbac84e3d818042916c24ecc492
Akuivan13 <riskiivan132@gmail.com>
2024-06-23 09:25:41 +0700

    4

--- index.js
+++ index.js
@@ -13,16 +13,19 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
+
 const readJSON = (filePath) => {
-  const fullPath = path.join(__dirname, filePath.replace(/^\.\//, ''));
-  return JSON.parse(fs.readFileSync(fullPath, 'utf8'));
+  try {
+    return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  } catch (error) {
+    console.error(`Error reading file from disk: ${error}`);
+    return [];
+  }
 };
 
 const writeJSON = (filePath, data) => {
-  const fullPath = path.join(__dirname, filePath.replace(/^\.\//, ''));
-  fs.writeFileSync(fullPath, JSON.stringify(data, null, 2));
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
 };
-
 
 app.get('/', (req, res) => {
   res.render('login');
@@ -34,36 +37,20 @@ app.get('/signup', (req, res) => {
 
 app.post('/signup', (req, res) => {
   const { username, password } = req.body;
-  let users;
-
-  try {
-    users = readJSON('./data/users.json');
-  } catch (error) {
-    return res.status(500).send('Internal Server Error');
-  }
+  const users = readJSON('./data/users.json');
 
   if (users.find(u => u.username === username)) {
     res.render('signup', { error: 'Username already exists' });
   } else {
     users.push({ username, password });
-    try {
-      writeJSON('./data/users.json', users);
-    } catch (error) {
-      return res.status(500).send('Internal Server Error');
-    }
+    writeJSON('./data/users.json', users);
     res.redirect('/');
   }
 });
 
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
-  let users;
-
-  try {
-    users = readJSON('./data/users.json');
-  } catch (error) {
-    return res.status(500).send('Internal Server Error');
-  }
+  const users = readJSON('./data/users.json');
 
   const user = users.find(u => u.username === username && u.password === password);
   if (user) {
@@ -74,31 +61,16 @@ app.post('/login', (req, res) => {
 });
 
 app.get('/dashboard', (req, res) => {
-  let products;
-
-  try {
-    products = readJSON('./data/products.json');
-  } catch (error) {
-    return res.status(500).send('Internal Server Error');
-  }
-
+  const products = readJSON('./data/products.json');
   res.render('dashboard', { products });
 });
 
 app.get('/product/:id', (req, res) => {
-  let products;
-
-  try {
-    products = readJSON('./data/products.json');
-  } catch (error) {
-    return res.status(500).send('Internal Server Error');
-  }
-
+  const products = readJSON('./data/products.json');
   const product = products.find(p => p.id === parseInt(req.params.id));
   if (!product) {
     return res.status(404).send('Product not found');
   }
-
   res.render('product', { product });
 });
 
